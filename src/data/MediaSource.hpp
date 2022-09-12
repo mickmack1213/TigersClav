7698b83fd2e8c01d42207d77693e277e25a04c75
Lixfel <agga-games@gmx.de>
2022-09-12 20:55:58 +0200

    Add -fpermissive under linux for newer ffmpeg support

--- src/data/MediaSource.hpp
+++ src/data/MediaSource.hpp
@@ -64,13 +64,13 @@ private:
 
     AVFormatContext* pFormatContext_;
 
-    const AVCodec* pVideoCodec_;
+    AVCodec* pVideoCodec_;
     AVStream* pVideoStream_;
     AVCodecContext* pVideoCodecContext_;
     enum AVPixelFormat hwPixFormat_;
     AVBufferRef *pHwDeviceContext_;
 
-    const AVCodec* pAudioCodec_;
+    AVCodec* pAudioCodec_;
     AVStream* pAudioStream_;
     AVCodecContext* pAudioCodecContext_;
 
